f2cd9ebbd0f743ddbe5003e85fb86281b687a8ca
Eric.Lee <a23034@athena.com.tw>
2024-10-11 17:39:44 +0800

    嘗試將開發用db放在容器內

---
+++ src/main/java/io/github/yienruuuuu/Application.java
@@ -0,0 +1,27 @@
+package io.github.yienruuuuu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@EnableScheduling
+@EnableCaching
+@EnableAsync
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+}
