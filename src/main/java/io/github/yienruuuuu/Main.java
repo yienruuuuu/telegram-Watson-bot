a8229eaf6619fc5625e4073be64fdd84616db3e0
Eric.Lee <a23034@athena.com.tw>
2024-10-11 13:57:29 +0800

    init

---
+++ src/main/java/io/github/yienruuuuu/Main.java
@@ -0,0 +1,11 @@
+package io.github.yienruuuuu;
+
+/**
+ * @author Eric.Lee
+ * Date: 2024/10/11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
