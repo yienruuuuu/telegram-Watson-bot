3ae787b8c064befba5a78c1c848f6f88a4c30d91
Eric.Lee <a23034@athena.com.tw>
2024-10-18 17:06:59 +0800

    修改為預設指令及按鍵鍵盤

--- src/main/java/io/github/yienruuuuu/service/application/telegram_bot/DivinationTelegramBot.java
+++ src/main/java/io/github/yienruuuuu/service/application/telegram_bot/DivinationTelegramBot.java
@@ -7,11 +7,9 @@ import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateC
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
-import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.util.Comparator;
-import java.util.List;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 /**
  * @author Eric.Lee Date: 2024/10/16
@@ -28,70 +26,45 @@ public class DivinationTelegramBot implements LongPollingSingleThreadUpdateConsu
 
     @Override
     public void consume(Update update) {
-        if (!update.hasMessage()) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            log.warn("無文字訊息存在, update: {}", update);
             return;
         }
+
         long chatId = update.getMessage().getChatId();
+        JsonUtils.parseJsonAndPrintLog("收到訊息", update);
+        String messageText = update.getMessage().getText();
 
-        // 檢查更新是否有消息並且訊息有文本
-        if (update.getMessage().hasText()) {
-            JsonUtils.parseJsonAndPrintLog("收到訊息", update);
-            String messageText = update.getMessage().getText();
-
-            if (messageText.equals("/start")) {
-                SendMessage message = SendMessage.builder()
-                        .chatId(chatId)
-                        .text(messageText)
-                        .build();
-                telegramBotClient.sendTextMessage(message);
-            } else if (messageText.equals("/pic")) {
-                SendPhoto message = SendPhoto
-                        .builder()
-                        .chatId(chatId)
-                        .photo(new InputFile("https://png.pngtree.com/background/20230519/original/pngtree-this-is-a-picture-of-a-tiger-cub-that-looks-straight-picture-image_2660243.jpg"))
-                        .caption("This is a little cat :)")
-                        .showCaptionAboveMedia(true)
-                        .build();
-                telegramBotClient.sendPhotoMessage(message);
-            } else {
-                SendMessage message = SendMessage
-                        .builder()
-                        .chatId(chatId)
-                        .text("Unknown command")
-                        .build();
-                telegramBotClient.sendTextMessage(message); // Sending our message object to user
-            }
-        } else if (update.getMessage().hasPhoto()) {
-            List<PhotoSize> photos = update.getMessage().getPhoto();
-            String fileId = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
-                    .map(PhotoSize::getFileId)
-                    .orElse("");
-            // Know photo width
-            int fileWidth = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
-                    .map(PhotoSize::getWidth)
-                    .orElse(0);
-            // Know photo height
-            int fileHeight = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
-                    .map(PhotoSize::getHeight)
-                    .orElse(0);
-            // Set photo caption
-            String caption = "file_id: " + fileId + "\nwidth: " + Integer.toString(fileWidth) + "\nheight: " + Integer.toString(fileHeight);
-            SendPhoto msg = SendPhoto
+        if (messageText.equals("/start")) {
+            SendMessage message = SendMessage
                     .builder()
                     .chatId(chatId)
-                    .photo(new InputFile(fileId))
-                    .caption(caption)
-                    .hasSpoiler(true)
+                    .text("歡迎使用yuki的每日占卜機器人，請輸入 /pic 來查看今日占卜結果。")
                     .build();
-//            InputPaidMedia med = new InputPaidMediaPhoto(fileId, false, "mediaaa", null, null);
-//            SendPaidMedia msg2 = SendPaidMedia
-//                    .builder()
-//                    .chatId(chatId)
-//                    .media(List.of(med,med))
-//                    .caption(caption)
-//                    .starCount(1)
-//                    .build();
-            telegramBotClient.sendPhotoMessage(msg);
+
+            // 將鍵盤加入訊息中
+            message.setReplyMarkup(ReplyKeyboardMarkup
+                    .builder()
+                    .keyboardRow(new KeyboardRow("/pic"))
+                    .build());
+
+            telegramBotClient.sendTextMessage(message);
+        } else if (messageText.equals("/pic")) {
+            SendPhoto message = SendPhoto
+                    .builder()
+                    .chatId(chatId)
+                    .photo(new InputFile("https://png.pngtree.com/background/20230519/original/pngtree-this-is-a-picture-of-a-tiger-cub-that-looks-straight-picture-image_2660243.jpg"))
+                    .caption("This is a little cat :)")
+                    .showCaptionAboveMedia(true)
+                    .build();
+            telegramBotClient.sendPhotoMessage(message);
+        } else {
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text("Unknown command")
+                    .build();
+            telegramBotClient.sendTextMessage(message); // Sending our message object to user
         }
     }
 }
